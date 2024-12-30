bb7ee1a0198fb7eeee15611c6b1301b709083926
HamSlice7 <jhambly@uoguelph.ca>
2024-12-30 13:43:52 -0500

    Creating a SummarizedExperiment object

--- scripts/tutorial_script.R
+++ scripts/tutorial_script.R
@@ -26,3 +26,38 @@ coldata <- read.csv("data/GSE96870_coldata_cerebellum.csv", row.names = 1)
 
 dim(coldata)
 
+#Reading in the gene annotation data
+rowranges <- read.delim("data/GSE96870_rowranges.tsv",
+                        sep = "\t",
+                        colClasses = c(ENTREZID = "character"),
+                        header = TRUE,
+                        quote = "",
+                        row.names = 5)
+dim(rowranges)
+
+#Checking the types of gene and the number of each
+table(rowranges$gbkey)
+
+#Making sure the sample ID's are in the same order for the counts data and the sample annotation data
+all.equal(colnames(counts), rownames(coldata))
+
+#Making sure the genes are in the same order for the counts data and the gene annotation data
+all.equal(rownames(counts), rownames(rowranges))
+
+# If the first is not TRUE, you can match up the samples/columns in
+# counts with the samples/rows in coldata like this (which is fine
+# to run even if the first was TRUE):
+tempindx <- match(colnames(counts), rownames(coldata))
+coldata <- coldata[tempindx,]
+
+#Check again:
+all.equal(colnames(counts), rownames(coldata))
+
+#One final check:
+stopifnot(colnames(count) == rownames(coldata),
+          rownames(counts) == rownames(rowranges))
+
+#Creating the SummarizedExperiment object
+se <- SummarizedExperiment(assays = list(counts),
+                           rowRanges = as(rowranges, "GRanges"),
+                           colData = coldata)
