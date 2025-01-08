0f01db0434d12facae4e35d1e0aa182afbcd56cc
HamSlice7 <jhambly@uoguelph.ca>
2025-01-08 08:41:03 -0500

    Working on tutorial 6

--- scripts/tutorial_script_episode_6.R
+++ scripts/tutorial_script_episode_6.R
@@ -65,3 +65,36 @@ meta_noninf
 vd <- VisualizeDesign(sampleData = meta_noninf, designFormula = ~ 0 + sex_tissue)
 vd$designmatrix
 vd$plotlist
+
+#testing for differences between tissues after accounting for baseline differences between mice for the female non-infected mice
+meta_fem_day0 <- meta %>% 
+  filter(sex == "Female" & time == "Day0")
+
+#ensure that mouse is treated as a categorical variable 
+meta_fem_day0$mouse <- factor(meta_fem_day0$mouse)
+
+vd <- VisualizeDesign(sampleData = meta_fem_day0, designFormula = ~ mouse + tissue)
+
+vd$designmatrix
+
+vd$plotlist
+
+#Subsetting for female mice and time (day0 and day4 mice)
+meta_fem_day_04 <- meta %>% 
+  filter(sex == "Female" & time %in% c("Day0", "Day4")) %>% 
+  droplevels()
+
+# ensure that mouse is treated as a categorical variable
+meta_fem_day_04$mouse <- factor(meta_fem_day_04$mouse)
+
+#Creating a design matrix with mouse number base line and the features
+design <- model.matrix(~ mouse, data = meta_fem_day_04)
+
+#Logical vectors are converted to numeric (TRUE = 1, FALSE = 0) when added to a matrix.the = operator is used within the cbind() function to name the new columns being added to the matrix. Adding spinalcord day 0 and day 4 features to the design matrix
+design <- cbind(design, Spc.Day0 = meta_fem_day_04$tissue == "Spinalcord" & meta_fem_day_04$time == "Day0", Spc.Day4 = meta_fem_day_04$tissue == "Spinalcord" & meta_fem_day_04$time == "Day4")
+
+#Validating the experimental design for comparing the two tissues in each infection group (Day0 and Day4), and contrast the tissue differences between the infection groups
+vd <- VisualizeDesign(sampleData = meta_fem_day_04 %>% 
+                        select(time, tissue, mouse), designMatrix = design)
+vd$designmatrix
+vd$plotlist
